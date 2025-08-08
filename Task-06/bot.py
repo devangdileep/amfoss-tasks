12bd452c7e0789c5d50c41eec151fb5dee9bfb6b
devangdileep <devangdileep123@gmail.com>
2025-08-09 00:27:44 +0530

    Added lyrics command to the bot

--- Task-06/bot.py
+++ Task-06/bot.py
@@ -1,4 +1,5 @@
 import os
+import aiohttp
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -11,6 +12,19 @@ bot = commands.Bot(command_prefix='/', intents=intents , help_command = None)
 async def help(ctx):
     embed = discord.Embeded(title = "Help Commands" , description = "Under Construction")
     await ctx.send(embed)
+
+@bot.command()
+async def lyrics(ctx, *, input1):
+    l = input1.split('-', 1)
+    artist = l[0].strip()
+    song = l[1].strip()
+    url = f"https://api.lyrics.ovh/v1/{artist}/{song}"
+    async with aiohttp.ClientSession() as searching:
+        async with searching.get(url) as data:
+            data = await data.json()
+    lyrics = data.get("lyrics")
+    await ctx.send(lyrics)
+
 
 load_dotenv() 
 token = os.getenv("TOKEN")
